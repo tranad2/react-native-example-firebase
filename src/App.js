b32c0f472f4fc7c8cdf418a4c408471b25cfbcf5
tranad2 <atran116@gmail.com>
2018-10-11 22:04:23 -0700

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+    render() {
+        <View>
+            <Text>An App</Text>
+        </View>
+    };
+}
+
+export default App;
