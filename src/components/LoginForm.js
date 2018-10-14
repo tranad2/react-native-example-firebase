72572e7a5e893ccfc1fb73fd36d4fdb90ea2d69e
tranad2 <atran116@gmail.com>
2018-10-13 19:49:11 -0700

    Add login form

---
+++ src/components/LoginForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { TextInput } from 'react-native';
+import { Button, Card, CardSection, Input } from './common';
+
+class LoginForm extends Component {
+state = { text: '' };
+
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <Input
+                        value={this.state.text}
+                        onChangeText={text => this.setState({ text })} 
+                    />
+                </CardSection>
+
+                <CardSection />
+
+                <CardSection>
+                    <Button>
+                        Log in
+                    </Button>
+                </CardSection>
+
+            </Card>
+        );
+    }
+}
+
+export default LoginForm;
---
+++ src/components/common/Input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+
+const Input = ({ label, value, onChangeText }) => {
+    return (
+        <View>
+            <Text>
+                {label}
+            </Text>
+
+            <TextInput
+                value={value}
+                onChangeText={onChangeText}
+                style={{ height: 40, width: 100 }}
+            />
+        </View>
+    )
+};
+
+export { Input };
